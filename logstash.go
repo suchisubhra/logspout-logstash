0e2fbbb58f978a84b135e0b1ad623f146faa40d8
David Keijser <david.keijser@klarna.com>
2015-12-04 15:42:41 +0100

    reconnect

--- logstash.go
+++ logstash.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"reflect"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/gliderlabs/logspout/router"
 )
@@ -17,12 +19,25 @@ func init() {
 
 // LogstashAdapter is an adapter that streams UDP JSON to Logstash.
 type LogstashAdapter struct {
+	mu    sync.Mutex
 	conn  net.Conn
 	route *router.Route
 }
 
 // NewLogstashAdapter creates a LogstashAdapter with UDP as the default transport.
 func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
+	conn, err := connect(route)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LogstashAdapter{
+		route: route,
+		conn:  conn,
+	}, nil
+}
+
+func connect(route *router.Route) (net.Conn, error) {
 	transport, found := router.AdapterTransports.Lookup(route.AdapterTransport("udp"))
 	if !found {
 		return nil, errors.New("unable to find adapter: " + route.Adapter)
@@ -41,10 +56,22 @@ func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
 		}
 	}
 
-	return &LogstashAdapter{
-		route: route,
-		conn:  conn,
-	}, nil
+	return conn, nil
+}
+
+func (a *LogstashAdapter) maybeReconnect(writeErr error) error {
+	time.Sleep(500 * time.Millisecond)
+	conn, err := connect(a.route)
+	if err != nil {
+		return err
+	}
+
+	a.mu.Lock()
+	a.conn.Close()
+	a.conn = conn
+	a.mu.Unlock()
+
+	return nil
 }
 
 func envLookup(env []string, key string) string {
@@ -81,9 +108,14 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 		}
 
 		_, err = a.conn.Write(js)
-		if err != nil {
+		for err != nil {
 			log.Println("logstash:", err)
-			continue
+			err = a.maybeReconnect(err)
+			if err != nil {
+				log.Println("logstash:", err)
+				break
+			}
+			_, err = a.conn.Write(js)
 		}
 	}
 }
